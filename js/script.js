11f31fbdf4f7287fddfe54707f3e06dddfa74ab9
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-10 16:25:12 +0200

    color

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-const myContainer = document.createElement('section')
-const square = document.querySelector('.fizz-buzz');
-square.className = "square"
+const myContainer = document.querySelector('.row');
 
-// CICLO PER NUMERI DA 0 a 10
+// CICLO PER NUMERI DA 0 a 100
 for (let i = 0; i <= 100; i++) {
+    const mySquare = document.createElement('div')
+
     // PRIMO CICLO DIVISIBILE per 3 e 5
     if (i % 3 === 0 && i % 5 === 0) {
-        square.innerHTML += "<div>fizzbuzz</div>";
+        // square.innerHTML += "<div>fizzbuzz</div>";
+        mySquare.append('fizzbuzz');
+        mySquare.className = 'my-square my-bg-blue';
+        myContainer.append(mySquare);
         console.log('FizzBuzz');
     } else if (i % 3 === 0) {
         // SECONDO CICLO DIVISIBILE per 3
-        square.innerHTML += "<div>fizz</div>";
+        // square.innerHTML += "<div>fizz</div>";
+        mySquare.append('fizz');
+        mySquare.className = 'my-square my-bg-light-blue';
+        myContainer.append(mySquare);
         console.log('Fizz');
     } else if (i % 5 === 0) {
         // TERZO CICLO DIVISIBILE per 5
-        square.innerHTML += "<div>buzz</div>";
+        // square.innerHTML += "<div>buzz</div>";
+        mySquare.append('buzz');
+        mySquare.className = 'my-square my-bg-lile';
+        myContainer.append(mySquare);
         console.log('Buzz');
     } else {
-        // ULTIMO CICLO NEL CASO NON FOSSERO DIVISIBILI PER 3 e 5
-        square.innerHTML += "<div>" + i + "</div>"
+        // ULTIMO CICLO NEL CASO NON FOSSERO DIVISIBILI PER 3 o 5
+        // square.innerHTML += "<div>" + i + "</div>"
+        mySquare.append(i);
+        mySquare.className = 'my-square';
+        myContainer.append(mySquare);
         console.log(i);
     }
-    console.log(square);
+    console.log(i);
 };
