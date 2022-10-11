3cd61ccccb7654e8a6e252e0faece92e5ea91f78
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-11 10:18:00 +0200

    snellimento codice

--- js/script.js
+++ js/script.js
@@ -3,30 +3,26 @@ const myContainer = document.querySelector('.row');
 // CICLO PER NUMERI DA 0 a 100
 for (let i = 0; i <= 100; i++) {
     const mySquare = document.createElement('div')
+    mySquare.className = 'my-square';
 
     if (i % 3 === 0 && i % 5 === 0) {
         // PRIMO CICLO DIVISIBILE per 3 e 5
-        // square.innerHTML += "<div>fizzbuzz</div>";
         mySquare.textContent = "Fizzbuzz";
-        mySquare.className = 'my-square my-bg-blue';
+        mySquare.classList.add('fizzbuzz');
         console.log('FizzBuzz');
     } else if (i % 3 === 0) {
         // SECONDO CICLO DIVISIBILE per 3
-        // square.innerHTML += "<div>fizz</div>";
         mySquare.textContent = "Fizz";
-        mySquare.className = 'my-square my-bg-light-blue';
+        mySquare.classList.add('fizz');
         console.log('Fizz');
     } else if (i % 5 === 0) {
         // TERZO CICLO DIVISIBILE per 5
-        // square.innerHTML += "<div>buzz</div>";
         mySquare.textContent = "Buzz";
-        mySquare.className = 'my-square my-bg-lile';
+        mySquare.classList.add('buzz');
         console.log('Buzz');
     } else {
         // ULTIMO CICLO NEL CASO NON FOSSERO DIVISIBILI PER 3 o 5
-        // square.innerHTML += "<div>" + i + "</div>"
-        mySquare.append(i);
-        mySquare.className = 'my-square';
+        mySquare.textContent = i;
     }
 
     myContainer.append(mySquare);
